ab1b2bb911a5510a1cc3bf381605c973190df337
manralhemant10 <manralhemant10@gmail.com>
2022-12-22 08:16:42 +0530

    added coding profile in nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,6 +10,8 @@ const Nav =()=> {
                     <NavLink activeClassName="navActive"  className="nav-item" to="/experience">Experience</NavLink> 
                     <NavLink activeClassName="navActive"  className="nav-item" to="/projects">Projects</NavLink> 
                     <NavLink activeClassName="navActive"  className="nav-item" to="/achievements">Achievements</NavLink> 
+                    <NavLink activeClassName="navActive"  className="nav-item" to="/codingprofile">Coding Profile</NavLink> 
+
                 </nav>
             </div>
     );
